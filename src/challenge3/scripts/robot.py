979e0b19c4fc75b3e14be4373dfc412ae10bed76
iiarroyo <israel.iap@outlook.com>
2023-05-08 13:51:37 -0600

    Moved robot class to robot.py

--- src/challenge3/scripts/robot.py
+++ src/challenge3/scripts/robot.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python  
 import rospy
-from geometry_msgs.msg import Twist, PoseStamped 
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Quaternion
 from std_msgs.msg import Float32 
 from sensor_msgs.msg import LaserScan  
 import numpy as np 
 import my_constants as constants
+from tf.transformations import quaternion_from_euler
 
 class Robot(): 
     '''
@@ -13,12 +15,20 @@ class Robot():
     
     def __init__(self):
         rospy.Subscriber("wl", Float32, self.wl_cb)  
-        rospy.Subscriber("wr", Float32, self.wr_cb) 
+        rospy.Subscriber("wr", Float32, self.wr_cb)
+        self.pose_pub = rospy.Publisher('puzzlebot_pose', Pose, queue_size=10)  
         self.x     = 0.0 # X position of the robot [m] 
         self.y     = 0.0 # Y position of the robot [m] 
-        self.theta = 0.0 # Angle of the robot [rad] 
+        self.theta = 0.0 # Angle of the robot [rad]
+        self.wl = 0.0
+        self.wr = 0.0
+        rate = rospy.Rate(constants.node_freq) #freq Hz  
         while not rospy.is_shutdown():
-            self.update_state()
+            self.update_state(self.wr,
+                              self.wl,
+                              constants.deltat)
+            self.publish_pose()
+            rate.sleep()
 
     def update_state(self, wr, wl, delta_t): 
         '''
@@ -27,13 +37,23 @@ class Robot():
         and returns the robot's state 
         '''
         v=constants.r*(wr+wl)/2 
-        w=constants.r*(wr-wl)/constants.L   
+        w=constants.r*(wr-wl)/constants.L
+
         self.theta=self.theta + w*delta_t 
         self.theta=np.arctan2(np.sin(self.theta),np.cos(self.theta)) 
         vx=v*np.cos(self.theta) 
         vy=v*np.sin(self.theta)
-        self.x=self.x+vx*delta_t
-        self.y=self.y+vy*delta_t
+        self.x+=vx*delta_t
+        self.y+=vy*delta_t
+
+        self.pose_pub()
+
+    def publish_pose(self):
+        msg = Pose()
+        msg.position.x = self.x
+        msg.position.y = self.y
+        msg.orientation = Quaternion(*quaternion_from_euler(0.0, 0.0, self.theta))
+        self.pose_pub(msg)
 
     def wl_cb(self, wl):  
         '''
@@ -45,4 +65,9 @@ class Robot():
         '''
         This function receives a the right wheel speed.  
         '''
-        self.wr = wr.data 
+        self.wr = wr.data
+
+
+if __name__ == "__main__":  
+    rospy.init_node("robot", anonymous=True)  
+    Robot()
